afe008a17c8d2b95a1459ef7c9f1b7b6ba97b8b9
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-08-23 14:58:29 +0200

    Good setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['cinplaAnalysis'],
     install_requires=[
     'aiohttp',
-    'allensdk',
+    'allensdk>=2.16',
     'appnope',
     'argon2-cffi',
     'argon2-cffi-bindings',
