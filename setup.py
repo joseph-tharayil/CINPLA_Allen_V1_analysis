21fa71d2ebaae4ac3aecbb93ece5bea1c7e9d071
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-08-23 13:07:50 +0200

    Reverst setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['cinplaAnalysis'],
     install_requires=[
     'aiohttp',
-    'allensdk>=2.16',
+    'allensdk',
     'appnope',
     'argon2-cffi',
     'argon2-cffi-bindings',
